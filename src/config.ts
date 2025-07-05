d10ee57f5e7f526d0533ae7cd59985d964c5ba8c
bupd <bupdprasanth@gmail.com>
2025-07-05 06:49:19 +0530

    feat: update api to use secure env variables
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Centralized configuration for the Harbor MCP tool.
- */
-export const harborConfig = {
-  apiBase: "https://demo.goharbor.io/api/v2.0",
-  authUser: "harbor-cli",
-  authPass: "Harbor12345",
-};
